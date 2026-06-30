54484a15c48b272fd59b7cefdb6e98cf687baa16
ramyavino2526-debug <ramyavino2526@gmail.com>
2026-06-30 17:31:09 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickMeBtn').addEventListener('click', function() {
+    const outputMessage = document.getElementById('outputMessage');
+    outputMessage.textContent = "Awesome! You are ready to push this to GitHub!";
+});
